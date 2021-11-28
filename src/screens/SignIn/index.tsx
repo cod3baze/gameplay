27fa36abda0f241533290fc6b34fcda17a92af76
elias alexandre <elliasallex@gmail.com>
2021-11-28 19:58:12 -0300

    initial commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Elias alexandre</Text>
+    </View>
+  );
+}
+
+export { SignIn };
